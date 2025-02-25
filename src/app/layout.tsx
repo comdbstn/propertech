4174ba7c8fcc846c6f5b5cbd25b2893ce5209e49
Your Name <your.email@example.com>
2025-02-26 05:36:00 +0900

    fix: 카카오맵 API 초기화 로직 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Noto_Serif_KR, Noto_Sans_KR } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
+import Script from "next/script";
 
 const serif = Noto_Serif_KR({ 
   subsets: ['latin'],
@@ -27,6 +28,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko" className={`${serif.variable} ${sans.variable}`}>
+      <head>
+        <Script
+          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_API_KEY}&autoload=false`}
+          strategy="beforeInteractive"
+        />
+      </head>
       <body className={sans.className}>
         <Providers>{children}</Providers>
       </body>
